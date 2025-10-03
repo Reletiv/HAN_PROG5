5f4179273bef9ac72ec1d73a6b5f268488971046
Wietse Houwers <wietsehouwers@gmail.com>
2025-10-03 20:53:16 +0200

    Add test code for pi

---
+++ Opdracht_4/main.cpp
@@ -0,0 +1,18 @@
+/*!
+ * \file      main.cpp
+ * \brief     Main entry point for BME280 Test code
+ * \author    Wietse Houwers
+ * \date      September 2025
+ *
+ * \details
+ * This file contains the setup and main loop for the BME280 Test code.
+ * It initializes hardware modules, handles periodic data polling, and writing results over serial
+ *
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Raspberry Pi!" << std::endl;
+    return 0;
+}
